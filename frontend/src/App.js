a048499c14c9ed4914793dd501147e2aec77a509
Mattis Tourneret <mattis.tourneret12@gmail.com>
2025-07-27 22:07:38 +0000

    Correction de la page blanche et ajout des routes dans App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ function App() {
           <Route path="/guidelines" element={<Guidelines />} />
         </Routes>
       </BrowserRouter>
+      <h1>Bienvenue sur AshenFall</h1>
     </div>
   );
 }
